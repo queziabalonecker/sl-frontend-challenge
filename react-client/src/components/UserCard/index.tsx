3da2312a17bf2988cd95eaf6ee966dfd917378fb
Quezia <queziaamenezes@gmail.com>
2022-07-29 21:50:03 -0300

    finishing user card

--- react-client/src/components/UserCard/index.tsx
+++ react-client/src/components/UserCard/index.tsx
@@ -1,11 +1,79 @@
 import { User } from '../../types/User';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-function UserCard({ user }: { user: User }) {
-  return (
-    <div className='user-card'>
-      <li>{user.name}</li>
-    </div>
-  );
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
 }
 
-export default UserCard;
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function UserCard({ user }: { user: User }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card key={user.id} sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
+            {user.name[0]}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label='settings'>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={user.name}
+        subheader={user.email}
+      />
+      <CardMedia
+        component='img'
+        height='194'
+        image={user.avatar}
+        alt='user name'
+      />
+      <CardContent></CardContent>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label='show more'>
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Phone:</Typography>
+          <Typography paragraph>{user.phone}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
